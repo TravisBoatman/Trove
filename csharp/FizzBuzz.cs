951c7b5053cbff5a1c5411c6beb3b108bb344ade
Travis Boatman <TravisBoatman@users.noreply.github.com>
2017-07-31 19:14:18 -0700

    Add FizzBuzz example

---
+++ csharp/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                var result = IsDivisble(i, 3, 5);
+                
+                if (result.Item1 && result.Item2)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (result.Item1)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (result.Item2)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static Tuple<bool, bool> IsDivisble(int x, int n, int y)
+        {
+            return new Tuple<bool, bool>(x % n == 0, x % y == 0);
+        }
+    }
+}
